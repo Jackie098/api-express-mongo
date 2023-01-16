6ba8cd24c9e65c6a89e52cb5feef1ec91bd9a0aa
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-01-16 16:43:25 -0300

    created jwt token to keep session

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,5 +1,7 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
 const UserModel = require("../models/User");
 
 const router = express.Router();
@@ -22,6 +24,43 @@ router.post("/register", async (req, res) => {
     message: "Registred with sucess!",
     data: user,
   });
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      error: true,
+      message: "User not found",
+    });
+  }
+
+  console.log(user);
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({
+      error: true,
+      message: "Password doesnt match",
+    });
+  }
+
+  user.password = undefined;
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+    },
+    authConfig.secret,
+    {
+      expiresIn: 24 * 60 * 60, //86400seg or 1 day
+    }
+  );
+
+  return res.json({ user, token });
 });
 
 module.exports = router;
